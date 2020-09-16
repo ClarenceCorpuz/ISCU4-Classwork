749a3e7bed9191bd7776a57e3e8fc1cbf1659d09
ClarenceCorpuz <47527535+ClarenceCorpuz@users.noreply.github.com>
2020-09-16 12:06:03 -0400

    Create 07.02 Dicts: Create Person.py

---
+++ Solutions/Repl.it/07.02 Dicts: Create Person.py
@@ -0,0 +1,11 @@
+from typing import Dict
+
+
+def create_person_dict(first_name: str, last_name:str):
+    dict = {
+        "first_name": f"{first_name}",
+        "last_name": f"{last_name}"
+    }
+    
+    return dict
+    

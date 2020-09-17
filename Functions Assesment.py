c206d0436342c4613a7266884176827ce8cc1029
ClarenceCorpuz <47527535+ClarenceCorpuz@users.noreply.github.com>
2020-09-17 14:36:04 -0400

    Update Functions Assesment.py

--- Functions Assesment.py
+++ Functions Assesment.py
@@ -1,1 +1,17 @@
+1. 
 
+def the_number_five():
+    return 5
+
+def so_useful(a):
+    return a
+
+def add_two(a):
+    return a + 2
+2. 
+
+def say_hello("Dave": str)
+
+def sum_list([4, 2, 6, 7]: List[int])
+
+def find_item_in_list("c": str, ["a", "b", "c"]: List[str])
